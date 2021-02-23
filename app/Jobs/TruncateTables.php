5b3ae9a9ad938c03268f8b7086b9282a0c1c48e5
Eric Ejimba <ejimba@gmail.com>
2021-02-23 07:16:01 +0300

    ETL process jobs 1

---
+++ app/Jobs/TruncateTables.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+
+class TruncateTables implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $databaseName;
+
+    public function __construct($databaseName)
+    {
+        $this->databaseName = $databaseName;
+    }
+
+    public function handle()
+    {
+        config(['database.connections.sqlsrv.database' => $this->databaseName]);
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_ARTPatients");
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_PatientLabs");
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_PatientPharmacy");
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_Patients");
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_PatientStatus");
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_PatientsWABWHOCD4");
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_PatientVisits");
+        DB::connection('sqlsrv')->statement("TRUNCATE TABLE stg_AdverseEvents");
+    }
+}
