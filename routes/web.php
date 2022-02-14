9c001d7655761f0d6086a03b522cc52a36738d28
cbrianbet <cbrianbet@outlook.com>
2022-02-14 09:38:12 +0300

    Updated url for test

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ten', function () {
+Route::get('/test', function () {
 
     $partners = Partner::get();
     $partnersNames = [];
