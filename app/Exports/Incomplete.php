f432ef0e7b40013c3b33d7c675510ba06c338d12
cbrianbet <cbrianbet@outlook.com>
2022-09-06 19:54:32 +0300

    fixed bug

--- app/Exports/Incomplete.php
+++ app/Exports/Incomplete.php
@@ -20,26 +20,30 @@ class Incomplete implements FromArray, WithHeadings, WithTitle, ShouldAutoSize, 
     public function map($row): array
     {
         return [
-            $row->{'MFL Code'},
+            $row->FacilityCode,
             $row->FacilityName,
-            $row->CTPartner,
-            $row->CTAgency,
-            $row->DateReceived,
-            $row->ExpectedPatients,
-            $row->Received
+            $row->SDP,
+            $row->County,
+            $row->avg_visits,
+            $row->current_no_of_visits,
+            $row->DateUploaded,
+            $row->DateQueryExecuted,
+            $row->percentage_of_avg_visits
         ];
     }
 
     public function headings(): array
     {
         return [
-            'MFL Code',
-            'Facility',
-            'Agency',
-            'Partner',
-            'Date Received',
-            'Expected Patients',
-            'Received'
+            'Facility Code',
+            'Facility Name',
+            'SDP',
+            'County',
+            'AVG Visits',
+            'Current No of Visits',
+            'Date Uploaded',
+            'Date Query Executed',
+            '% of AVG Visits'
         ];
     }
 
--- app/Exports/Stale.php
+++ app/Exports/Stale.php
@@ -20,30 +20,26 @@ class Stale implements FromArray, WithHeadings, WithTitle, ShouldAutoSize, WithM
     public function map($row): array
     {
         return [
-            $row->FacilityCode,
+            $row->{'MFL Code'},
             $row->FacilityName,
-            $row->SDP,
-            $row->County,
-            $row->avg_visits,
-            $row->current_no_of_visits,
-            $row->DateUploaded,
-            $row->DateQueryExecuted,
-            $row->percentage_of_avg_visits
+            $row->CTPartner,
+            $row->CTAgency,
+            $row->DateReceived,
+            $row->ExpectedPatients,
+            $row->Received
         ];
     }
 
     public function headings(): array
     {
         return [
-            'Facility Code',
-            'Facility Name',
-            'SDP',
-            'County',
-            'AVG Visits',
-            'Current No of Visits',
-            'Date Uploaded',
-            'Date Query Executed',
-            '% of AVG Visits'
+            'MFL Code',
+            'Facility',
+            'Agency',
+            'Partner',
+            'Date Received',
+            'Expected Patients',
+            'Received'
         ];
     }
 
